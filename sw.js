01084b3da01e4a8826fc066ddb919e250084125b
loicfarine-debug <loic.farine@bluewin.ch>
2026-08-18 14:57:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='cg-bar-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','styles.css','app.js','logo.png','icon-192.png','icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
